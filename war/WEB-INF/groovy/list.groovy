667d3b1b961980b3c9d885447c94930ae5846098
Seymour Cakes <seymores@gmail.com>
2011-04-24 13:49:10 +0800

    Added query with cursor

--- war/WEB-INF/groovy/list.groovy
+++ war/WEB-INF/groovy/list.groovy
@@ -4,22 +4,37 @@ import static com.google.appengine.api.datastore.FetchOptions.Builder.*
 
 //System.out.println params
 
-def bugs = null
+int PAGE_SIZE = 15
 
-if ('firstpage' in memcache) {
-    bugs = memcache['firstpage']    
+def bugs = null
+def startCursor = request.cursor
+
+if (('page'+startCursor) in memcache) {
+    bugs = memcache['page'+startCursor]    
+}
+
+def fetchOptions = FetchOptions.Builder.withLimit(PAGE_SIZE);
+if (startCursor) {
+    fetchOptions.startCursor(Cursor.fromWebSafeString(startCursor));
 }
 
 if (!bugs) {
     def query = new Query("bug")
     query.addSort("createDate", Query.SortDirection.DESCENDING)
-    //query.addSort("status", Query.SortDirection.DESCENDING)
+    query.addFilter("status", Query.FilterOperator.EQUAL, "open")
+   // query.addFilter("status", Query.FilterOperator.EQUAL, "reopen")
+  //  query.addFilter("status", Query.FilterOperator.IN, ["open", "reopen"])a //Not supported if using cursor
+    
     def preparedQuery = datastore.prepare(query)
-
-    bugs = preparedQuery.asList(withDefaults())
-    memcache['firstpage'] = bugs
+ //   bugs = preparedQuery.asList(withDefaults())
+    bugs = preparedQuery.asQueryResultList(fetchOptions);
+    startCursor = bugs.getCursor().toWebSafeString()
+    String c = "page-" + startCursor.toString() 
+ //   System.out.println bugs.toList()
+    memcache[c] = bugs.toList()
 }
 
+request.cursor = startCursor
 request.bugs = bugs
 
 //System.out.println bugs
